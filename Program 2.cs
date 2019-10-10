46bf632780de3a0eb52548d2aba48d85a5669c6b
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2019-10-10 08:43:24 +0100

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int life = 100;
+            int attack = 100;
+            attack = attack - life;
+            Console.WriteLine("gg");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+            Random r = new Random();
+            int health a1 = (1, 100);
+            int attack a2 = (1, 100);
+            int defense a3 = (1, 100);
+
+            Random r = new Random();
+            int health b1 = (1, 100);
+            int attack b2 = (1, 100);
+            int defense b3 = (1, 100);
+
+            Random r = new Random();
+            int  Media health = a1+b1\2;
+            int  Media attack  = a2+b2\2;
+            int Media defense  = a3+b3;
+
+
+            Console.WriteLine("Media health a1+ Media health b2");
+        }
+    }
+}
