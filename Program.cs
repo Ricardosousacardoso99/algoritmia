450ec5b6786c1a2cc5eec0daba349ad511318199
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2020-11-08 19:28:34 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,26 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
-
-
-
         {
             Random r = new Random();
-            int health a1 = (1, 100);
-            int attack a2 = (1, 100);
-            int defense a3 = (1, 100);
+            int counter = 0;
+            int n = r.Next(100);
+            int num;
 
-            Random r = new Random();
-            int health b1 = (1, 100);
-            int attack b2 = (1, 100);
-            int defense b3 = (1, 100);
+            for(int i= 0; i<n; i++)
+            {
+                num = r.Next(101);
 
-            Random r = new Random();
-            int  Media health = a1+b1\2;
-            int  Media attack  = a2+b2\2;
-            int Media defense  = a3+b3;
-
-
-            Console.WriteLine("Media health a1+ Media health b2");
-        }
+                if(num >50)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }    
     }
 }
