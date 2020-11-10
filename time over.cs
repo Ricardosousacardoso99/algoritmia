084777cb962c5037dfb04203d3f06530ed2b9659
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2020-11-10 22:27:15 +0000

    Add files via upload

---
+++ time over.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace time_over
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i=99; i>=0;i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("time_over");
+                }
+            }
+        }
+    }
+}
