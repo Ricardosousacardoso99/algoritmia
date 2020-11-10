8cd4476a1ca4658978cb25205687d9505a43c07d
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2020-11-10 22:48:39 +0000

    Add files via upload

---
+++ secret number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace secret_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int secretnumber = r.Next(0, 101);
+
+            int palpit = int.Parse(Console.ReadLine());
+
+            bool flag = true;
+
+            while(flag)
+            {
+                if (palpit > secretnumber)
+                {
+                    Console.WriteLine("menor");
+
+                }
+               else if (palpit < secretnumber)
+                {
+                    Console.WriteLine("maior");
+                }
+
+                else if (palpit==secretnumber)
+                {
+                    flag = false;
+                    Console.WriteLine("igual");
+                    break;
+                }
+                  
+                 palpit = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
