f98e96f50a338d47e97d4d24ffe65da2ec52334e
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2020-11-25 21:19:59 +0000

    Add files via upload

---
+++ Rool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Rool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int p1;
+            int p2;
+
+            p1 = r.Next(1, 6);
+            p2 = r.Next(1, 6);
+
+
+            if(p1<p2)
+            {
+                Console.WriteLine("Win");
+            }
+            else if (p2<p1)
+             {
+                Console.WriteLine("Win");
+            }
+            else
+            {
+                Console.WriteLine("Empate");
+            }
+        }
+    }
+}
