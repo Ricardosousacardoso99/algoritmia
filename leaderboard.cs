9afe264dbe5398c026a93d6e352fb93377e38e1c
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2020-11-25 21:01:00 +0000

    Add files via upload

---
+++ leaderboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace leaderboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int p1 = r.Next(1, 100);
+            int p2 = r.Next(1, 100);
+            int p3 = r.Next(1, 100);
+
+
+
+            int maior1 = p1;
+            int maior2 = p2;
+            int maior03 = p3;
+
+            if (p2 > maior1)
+            {
+                maior2 = maior1;
+                maior1 = p2;
+            }
+
+            if (p3 > maior1)
+            {
+                maior03 = maior2;
+                maior2 = maior1;
+                maior1 = p3;
+            }
+
+            else
+            {
+                if (p3 > maior2)
+                {
+                    maior03 = maior2;
+                    maior2 = p3;
+                }
+            }
+        }
+    }
+}
