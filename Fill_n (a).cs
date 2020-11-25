aab90cdf55180fb763717b56708a8244c23a53e8
Ricardosousacardoso99 <55951004+Ricardosousacardoso99@users.noreply.github.com>
2020-11-25 21:25:07 +0000

    Add files via upload

---
+++ Fill_n (a).cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fill_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[10];
+            int v = 10;
+
+            for(int i=0,i <= valores.Length,i++)
+            {
+                valores[i]= v
+                    v--;
+            }
+        }
+    }
+}
